bad43978f86476c5515107481b6dfb59a0340833
Alex9716 <51192166+Alex9716@users.noreply.github.com>
2020-12-22 20:48:28 +0100

    Add files via upload

---
+++ Alex_DNN.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+
+filepath = "DehnungVerschiebungMatrix.csv"
+df = pd.read_csv(filepath)
+h1 = np.array(df)
+# print(df)
+
+
+# Training set
+x = h1[:, 0:6]
+y = h1[:, 6:10]
+train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.4, random_state=75)
+
+
+# Create Keras model
+tf.keras.backend.clear_session()
+tf.random.set_seed(120)
+
+model = keras.models.Sequential([
+
+    keras.layers.Dense(2048, input_dim=6, activation='tanh'),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dropout(0.4),
+
+    keras.layers.Dense(1024, activation='tanh'),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dropout(0.3),
+
+    keras.layers.Dense(256, activation='tanh'),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dropout(0.2),
+
+    keras.layers.Dense(256, activation='tanh'),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dropout(0.2),
+
+    keras.layers.Dense(128, activation='tanh'),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dropout(0.015),
+
+    keras.layers.Dense(24, activation='tanh'),
+
+    keras.layers.Dense(4, activation="linear"),
+], name="Larger_network", )
+
+# Gradient descent algorithm
+optimizer = keras.optimizers.Adam(lr=0.0125, decay=5e-3)
+
+model.compile(optimizer=optimizer, warm_start=False,
+              loss='mean_absolute_error')
+
+history = model.fit(train_x, train_y,
+                    epochs=1250, batch_size=128,
+                    validation_data=(test_x, test_y),
+                    verbose=1)
+
+
+plt.plot(history.history['loss'])
+plt.xlabel("No. of Iterations")
+plt.ylabel("J(Theta1 Theta0)/Cost")
+plt.show()
+
+predict = np.array([[-165.070258009183, 0.0408137033058864, -161.315124372419,
+                     0.0398370454247239, -157.408731226767, 0.0389030402486873]])
+print(model.predict(predict))
